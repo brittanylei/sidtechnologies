d73c3e752334f9819a0ec3d36faae6dca14f36e6
brittanylei <brittlei48@gmail.com>
2018-02-04 18:01:17 -0800

    fixed indentation for classes

--- Back_End/sid.py
+++ Back_End/sid.py
@@ -1,24 +1,22 @@
 
-
 from django.db import models
 
 class Client(models.Model):
-  name = model.CharField(max_length=100)
-  email = model.EmailField()
-  phoneNum = model.CharField(max_length=15)
-  position = model.CharField(max_length=50)
+    name = model.CharField(max_length=100)
+    email = model.EmailField()
+    phoneNum = model.CharField(max_length=15)
+    position = model.CharField(max_length=50)
 
-  def __str__(self):
-    return self.name
-
+    def __str__(self):
+        return self.name
 
 class Sprint(models.Model):
-  projectName = model.CharField(max_length=50)
-  projectOwner = model.CharField(max_length=50)
-  scrumMaster = model.CharField(max_length=50)
-  team = model.CharField(max_length=50)
-  sprintNum = models.IntegerField(default=0)
-  sprintPlanDate = models.DateTimeField()
-  sprintRevDate = models.DateTimeField()
-  sprintRetroDate = models.DateTimeField()
-  scrumMeetDate = models.DateTimeField()
+    projectName = model.CharField(max_length=50)
+    projectOwner = model.CharField(max_length=50)
+    scrumMaster = model.CharField(max_length=50)
+    team = model.CharField(max_length=50)
+    sprintNum = models.IntegerField(default=0)
+    sprintPlanDate = models.DateTimeField()
+    sprintRevDate = models.DateTimeField()
+    sprintRetroDate = models.DateTimeField()
+    scrumMeetDate = models.DateTimeField()
