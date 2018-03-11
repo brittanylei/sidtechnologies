52c1146254a72c3f494ad5d69ce34dd6f46c1208
gdeguzma <gdeguzma@ucsc.edu>
2018-03-11 16:17:46 -0700

    the whole package

---
+++ Front_End/sss_package/functions/index.js
@@ -0,0 +1,17 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+var config = {
+      apiKey: "AIzaSyDX8In6v77C3gMQB6L99mF2PQyLBtul28g",
+      authDomain: "silver-surfer-services.firebaseapp.com",
+      databaseURL: "https://silver-surfer-services.firebaseio.com",
+      projectId: "silver-surfer-services",
+      storageBucket: "silver-surfer-services.appspot.com",
+      messagingSenderId: "272241968147"
+  };
